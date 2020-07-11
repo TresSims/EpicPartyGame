23d20775db2a08bea1ae85065b16c556444b665d
Tres Sims <tres.sims95@gmail.com>
2020-07-10 21:31:24 -0400

    Organized Project Structure
    Final
    Organized Project Structure
    Added Style Guide
    Organizationnnn

--- Source/EpicPartyGame/EpicPartyGameGameMode.cpp
+++ Source/EpicPartyGame/EpicPartyGameGameMode.cpp
@@ -1,15 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "EpicPartyGameGameMode.h"
-#include "EpicPartyGameCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AEpicPartyGameGameMode::AEpicPartyGameGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	
 }
